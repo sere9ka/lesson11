b62deb77cc7b9b0d919f09bd4c7c00ecf5709935
Sergey Kovylov <sere99ka@gmail.com>
2021-10-17 12:21:18 +0300

    выполнил задание

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
+'use strict'
 
+const btnButton = document.querySelector('#btn');
+const btnSquare = document.querySelector('#e_btn');
+const inputText = document.querySelector('#text');
+const square = document.querySelector('#square');
+const circle = document.querySelector('#circle');
+const range = document.querySelector('#range');
+const rangeSpan = document.querySelector('#range-span');
+let valuesInput;
 
+btnSquare.style.display = 'none';
 
+const valueInput = function (event) {
+    valuesInput = event.target.value;
+}
 
+inputText.addEventListener('input', valueInput);
+const bkgcSquare = function () {
+    square.style.backgroundColor = valuesInput
+}
+btnButton.addEventListener('click', bkgcSquare);
 
+const circleSize = function (event) {
+    valuesInput = event.target.value;
+    rangeSpan.textContent = valuesInput
+    circle.style.width = valuesInput + '%';
+    circle.style.height = valuesInput + '%';
+}
+range.addEventListener('input', circleSize);
